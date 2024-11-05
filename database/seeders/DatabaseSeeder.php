5b48e8307b93600cf62c71687164ab2b988d40ea
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2024-11-05 10:47:48 +0100

    Create a factory for Posts, and seed 5 users with 10 posts for each one

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()
+            ->count(5)
+            ->state([
+                'password' => '123456'
+            ])
+            ->hasPosts(10)
+            ->create();
     }
 }
